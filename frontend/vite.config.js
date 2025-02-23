2a1b51895d5f47edc84b6b14de878d1f0b9048d9
Ganapathi Subramanian <gana301104@gmail.com>
2025-02-23 13:03:27 +0530

    updated package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  server: {
-    historyApiFallback: true,
-  },
   build: {
     rollupOptions: {
       output: {
